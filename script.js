48245a9bf1e2d07d3923bb892db43444a4344c5d
zapallo <zatapallo@gmail.com>
2025-06-18 19:27:38 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/
+
+function verImagen(){
+    document.getElementById("imagen").innerHTML = "img src='https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/' "
+}
