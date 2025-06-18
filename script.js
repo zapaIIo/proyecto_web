2b84438bcc8eefc36915d3ca6128069d672d9dea
zapallo <zatapallo@gmail.com>
2025-06-18 19:43:11 -0400

    java_script_2

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/
-
-function verImagen(){
+// img_caso1 img src="https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/"
+// img_caso2
+//img_caso3
+function verCaso1(){
+    texto = '<img src="https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/"'
     document.getElementById("imagen").innerHTML = "img src='https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/' "
+}
+
+function verCaso2(){
+    document.getElementById("imagen").innerHTML = "img src='https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/' "
+}
+
+function verCaso3(){
+    document.getElementById("imagen").innerHTML = "img src='https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/' "
+}
+
+function sumar() {
+    var suma = 8 + 8;
+    document.getElementById('imagen').innerHTML = '<h1>' +suma+ '</h1>'
 }
