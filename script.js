75c0bb8c5d7ae56921d75e5a98cdf3dc16a97288
zapallo <zatapallo@gmail.com>
2025-06-22 21:49:47 -0400

    porcentajes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // img_caso1 img src="https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/"
 // img_caso2
-//img_caso3
+// img_caso3
 function verCaso1(){
     texto = '<img src="https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/"'
     document.getElementById("imagen").innerHTML = "img src='https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/' "
 }
 
 function verCaso2(){
-    document.getElementById("imagen").innerHTML = "img src='https://pixabay.com/es/photos/familia-sol-amor-abuelos-gente-7392843/' "
+    document.getElementById("imagen").innerHTML = "img src='https://cdn.pixabay.com/photo/2022/08/17/15/46/family-7392843_1280.jpg' "
 }
 
 function verCaso3(){
